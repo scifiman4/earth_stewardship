5105ce8b232ef7d0515abd9eca0be0786acf4ad0
Matthew Denaburg <matthew.denaburg@ssfs.org>
2017-01-17 11:12:41 -0500

    refactored Group to use BaseEntity class

--- src/org/ssfs/earth_stewardship/entities/Group.java
+++ src/org/ssfs/earth_stewardship/entities/Group.java
@@ -8,23 +8,16 @@ package org.ssfs.earth_stewardship.entities;
 /**
  * @author denabma
  */
-public class Group {
+public class Group extends BaseEntity {
 
-	private int groupId;
 	private String groupLeader;
 	private final Schedule mySchedule;
 
-	private static int nextGroupId = 0;
-
 	public Group(String leader) {
-		this.groupId = nextGroupId++;
+		super();
 
 		this.groupLeader = leader;
 		this.mySchedule = new Schedule();
-	}
-
-	int getGroupId() {
-		return groupId;
 	}
 
 	public String getGroupLeader() {
@@ -33,28 +26,6 @@ public class Group {
 
 	public Schedule getMySchedule() {
 		return new Schedule(mySchedule);
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + groupId;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Group other = (Group) obj;
-		if (groupId != other.groupId)
-			return false;
-		return true;
 	}
 
 	@Override

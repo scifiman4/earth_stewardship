8ac32a194f4bc203443419e1f0ecdc1b93eaab8d
Matthew Denaburg <matthew.denaburg1@gmail.com>
2017-01-20 09:31:30 -0500

    create schedule map

--- src/org/ssfs/earth_stewardship/entities/GroupSchedule.java
+++ src/org/ssfs/earth_stewardship/entities/GroupSchedule.java
@@ -4,22 +4,29 @@
 
 package org.ssfs.earth_stewardship.entities;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author denabma
  */
 public class GroupSchedule {
 
+    private int groupId;
+    private Map<ScheduleBlock, Session> schedule;
+
     /**
      * copy constructor TODO
-     * 
+     *
      * @param orig
      */
     GroupSchedule(GroupSchedule orig) {
 	// TODO create copy constructor
     }
 
-    public GroupSchedule() {
-	// TODO complete constructor
+    public GroupSchedule(int groupId) {
+	this.groupId = groupId;
+	schedule = new HashMap<ScheduleBlock, Session>();
     }
 
 }

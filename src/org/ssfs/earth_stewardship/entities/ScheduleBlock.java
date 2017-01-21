d494603d03d018bfafdf2b0be48453f46a7fe399
Matthew Denaburg <matthew.denaburg1@gmail.com>
2017-01-21 13:08:18 -0500

    added constuctor

--- src/org/ssfs/earth_stewardship/entities/ScheduleBlock.java
+++ src/org/ssfs/earth_stewardship/entities/ScheduleBlock.java
@@ -5,9 +5,20 @@
 package org.ssfs.earth_stewardship.entities;
 
 /**
- * @author denabma
+ * {@link Session}s are scheduled here.
  *
+ * @author denabma
  */
 public class ScheduleBlock {
+
+    private int blockId;
+    private String blockName;
+
+    private static int nextId = 0;
+
+    public ScheduleBlock(String name) {
+	blockId = nextId++;
+	blockName = name;
+    }
 
 }

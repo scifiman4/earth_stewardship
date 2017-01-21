ed5f0f757ec46107c4df69c9cb10dca21ab1cdc0
Matthew Denaburg <matthew.denaburg1@gmail.com>
2017-01-21 13:06:05 -0500

    javadocs + added id

--- src/org/ssfs/earth_stewardship/entities/GroupSchedule.java
+++ src/org/ssfs/earth_stewardship/entities/GroupSchedule.java
@@ -12,21 +12,34 @@ import java.util.Map;
  */
 public class GroupSchedule {
 
-    private int groupId;
+    /** unique id */
+    private int groupScheduleId;
 
+    /** the {@link Group} whose schedule this is */
+    private int groupId;
+    /** the schedule */
     private Map<ScheduleBlock, Session> schedule;
 
+    private static int nextId = 0;
+
+    private GroupSchedule() {
+	groupScheduleId = nextId++;
+    }
+
     public GroupSchedule(int groupId) {
+	this();
 	this.groupId = groupId;
 	schedule = new HashMap<ScheduleBlock, Session>();
     }
 
     /**
-     * copy constructor TODO
+     * copy constructor
      *
      * @param original
+     *            the original {@link GroupSchedule}
      */
     public GroupSchedule(GroupSchedule original) {
+	this();
 	groupId = original.groupId;
 	schedule = original.schedule;
     }

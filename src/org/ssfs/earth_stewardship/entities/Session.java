4fb0a4b01d9de4acf918527e60bbd73fd3a4fd7e
Matthew Denaburg <matthew.denaburg@ssfs.org>
2017-01-23 09:34:35 -0500

    constructor for Session.java

--- src/org/ssfs/earth_stewardship/entities/Session.java
+++ src/org/ssfs/earth_stewardship/entities/Session.java
@@ -5,8 +5,18 @@
 package org.ssfs.earth_stewardship.entities;
 
 /**
+ * This is a similar to a Section in a course scheduling situation.
+ * 
  * @author denabma
  */
 public class Session {
+
+    private int maxSize;
+    private String name;
+
+    public Session(int size, String name) {
+	maxSize = size;
+	this.name = name;
+    }
 
 }

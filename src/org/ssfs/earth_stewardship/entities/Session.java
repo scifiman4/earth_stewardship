3237df5232189bf26a91b0f5c4cbb9ac7dac8c14
Matthew Denaburg <matthew.denaburg1@gmail.com>
2017-01-28 10:25:33 -0500

    implemented most of Session

--- src/org/ssfs/earth_stewardship/entities/Session.java
+++ src/org/ssfs/earth_stewardship/entities/Session.java
@@ -1,20 +1,124 @@
-/**
- *
- */
-
 package org.ssfs.earth_stewardship.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * This is a similar to a Section in a course scheduling situation.
+ * This is a similar to a Section in a course scheduling situation. {link
+ * Group}s are scheduled into them.
  */
 public class Session {
 
-    private int maxSize;
-    private String name;
+    // ===== basic info about the Sessions =====
 
-    public Session(int size, String name) {
+    /** the name of this {@link Session}'s activity */
+    private String activityName;
+    /** the location this {@link Session} meets in */
+    private String location;
+    /**
+     * the maximum number of people in {@link Group}s that can be scheduled in
+     * this {@link Session}.
+     */
+    private int maxSize;
+
+    // ===== scheduling fields =====
+
+    /** the total number of people in each scheduled {@link Group} */
+    private int currentSize;
+    /** the {@link Group}s scheduled in this {@link Session} */
+    private Set<Group> scheduled;
+
+    // ===== Constructors =====
+
+    public Session() {
+	currentSize = 0;
+	scheduled = new HashSet<Group>();
+    }
+
+    /**
+     * @param maxSize
+     *            initial maxSize
+     * @param activityName
+     *            initial activityName for the {@link Session}
+     */
+    public Session(int maxSize, String activityName) {
+	this();
+	this.maxSize = maxSize;
+	this.activityName = activityName;
+    }
+
+    // ===== Accessor methods =====
+
+    public String getActivityName() {
+	return activityName;
+    }
+
+    public String getLocation() {
+	return location;
+    }
+
+    public int getMaxSize() {
+	return maxSize;
+    }
+
+    public int getCurrentSize() {
+	return currentSize;
+    }
+
+    public Set<Group> getScheduledGroups() {
+	return new HashSet<Group>(scheduled);
+    }
+
+    // ===== Setter methods =====
+
+    public void setActivityName(String name) {
+	activityName = name;
+    }
+
+    public void setLocation(String location) {
+	this.location = location;
+    }
+
+    public void setMaxSize(int size) {
 	maxSize = size;
-	this.name = name;
+    }
+
+    // ===== Overrides =====
+
+    @Override
+    public String toString() {
+	String ret = "Session [maxSize="
+		+ maxSize
+		+ ", currentSize="
+		+ currentSize
+		+ ", "
+		+ (activityName != null ? "activityName=" + activityName + ", "
+			: "");
+	if (scheduled != null) {
+	    ret += "scheduled:" + (scheduled.size() == 0 ? " None" : "");
+	    for (Group group : scheduled) {
+		ret += "\n" + group;
+	    }
+	}
+	return ret;
+    }
+
+    // ===== methods for scheduling =====
+
+    public void unschedule(Group group) {
+	if (scheduled.remove(group)) {
+	    currentSize -= group.getSize();
+	}
+    }
+
+    public void schedule(Group group) {
+	if (currentSize + group.getSize() <= maxSize && scheduled.add(group)) {
+	    currentSize += group.getSize();
+	}
+    }
+
+    public boolean isScheduled(Group group) {
+	return scheduled.contains(group);
     }
 
 }

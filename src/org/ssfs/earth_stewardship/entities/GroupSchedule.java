e6086c39a3f6324e4563a654cb81d0b3db0dc504
Matthew Denaburg <matthew.denaburg1@gmail.com>
2017-02-05 10:36:39 -0500

    update constructors

--- src/org/ssfs/earth_stewardship/entities/GroupSchedule.java
+++ src/org/ssfs/earth_stewardship/entities/GroupSchedule.java
@@ -9,31 +9,11 @@ import java.util.Map;
 
 public class GroupSchedule {
 
-    /** the {@link Group} whose schedule this is */
-    private int groupId;
     /** the schedule */
     private Map<ScheduleBlock, Session> schedule;
 
-    public GroupSchedule(int groupId) {
-	this.groupId = groupId;
+    public GroupSchedule() {
 	schedule = new HashMap<ScheduleBlock, Session>();
-    }
-
-    /**
-     * copy constructor
-     *
-     * @param original
-     *            the original {@link GroupSchedule}
-     */
-    public GroupSchedule(GroupSchedule original) {
-	groupId = original.groupId;
-	schedule = original.schedule;
-    }
-
-    @Override
-    public String toString() {
-	return "GroupSchedule ["
-		+ (schedule != null ? "schedule=" + schedule : "") + "]";
     }
 
 }

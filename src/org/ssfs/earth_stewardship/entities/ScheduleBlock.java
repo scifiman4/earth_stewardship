35a2c4b27aef316d71cbbcbfd99417724201d42f
Matthew Denaburg <matthew.denaburg1@gmail.com>
2017-02-05 10:37:52 -0500

    barebones of ScheduleBlock

--- src/org/ssfs/earth_stewardship/entities/ScheduleBlock.java
+++ src/org/ssfs/earth_stewardship/entities/ScheduleBlock.java
@@ -9,10 +9,16 @@ package org.ssfs.earth_stewardship.entities;
  */
 public class ScheduleBlock {
 
+    /** the name of this block */
     private String blockName;
+    /** the order for which to display this {@link ScheduleBlock} in */
+    private int blockNumber;
+
+    private static int nextBlockNumber = 0;
 
     public ScheduleBlock(String name) {
 	blockName = name;
+	blockNumber = nextBlockNumber++;
     }
 
 }

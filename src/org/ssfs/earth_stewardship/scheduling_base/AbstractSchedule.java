aa5024a1aec26d229bf937cac1eb11fd71192ba0
Matthew Denaburg <matthew.denaburg1@gmail.com>
2017-02-19 09:44:59 -0500

    reset for now

--- src/org/ssfs/earth_stewardship/scheduling_base/AbstractSchedule.java
+++ src/org/ssfs/earth_stewardship/scheduling_base/AbstractSchedule.java
@@ -1,20 +1,8 @@
 package org.ssfs.earth_stewardship.scheduling_base;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 /**
  * This is the schedule for an {@link AbstractSchedulable}.
  */
 public abstract class AbstractSchedule {
-
-    public AbstractSchedule copy() {
-	// TODO Auto-generated method stub
-	throw new NotImplementedException();
-    }
-
-    public void createFromTemplate(ScheduleTemplate template) {
-	// TODO Auto-generated method stub
-	throw new NotImplementedException();
-    }
 
 }

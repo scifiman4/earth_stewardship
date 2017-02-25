eb806cda19b66faa4238c9da42fb6969aab68a1b
Matthew Denaburg <matthew.denaburg1@gmail.com>
2017-02-25 09:27:47 -0500

    implementing stuff

--- src/org/ssfs/earth_stewardship/scheduling_base/AbstractSchedule.java
+++ src/org/ssfs/earth_stewardship/scheduling_base/AbstractSchedule.java
@@ -1,8 +1,35 @@
 package org.ssfs.earth_stewardship.scheduling_base;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This is the schedule for an {@link AbstractSchedulable}.
  */
-public abstract class AbstractSchedule {
+public class AbstractSchedule {
+
+    /**
+     * Maps a {@link Block} to an {@link Activity}.
+     */
+    protected Map<Block, Activity> schedule;
+
+    public AbstractSchedule() {
+	schedule = new HashMap<Block, Activity>();
+    }
+
+    public boolean isBlockScheduled(Block block) {
+	return schedule.containsKey(block) && schedule.get(block) != null;
+    }
+
+    public Activity getActivityInBlock(Block block) {
+	return isBlockScheduled(block) ? schedule.get(block) : null;
+    }
+
+    public void scheduleActivityIntoBlock(Block block, Activity activity) {
+	if (block == null || activity == null) {
+	    throw new NullPointerException();
+	}
+	schedule.put(block, activity);
+    }
 
 }

ef48911dd337a951660337b87a1e44d72e469a96
Matthew Denaburg <matthew.denaburg1@gmail.com>
2017-03-05 10:17:14 -0500

    Block

--- src/org/ssfs/earth_stewardship/scheduling_base/Block.java
+++ src/org/ssfs/earth_stewardship/scheduling_base/Block.java
@@ -4,7 +4,19 @@ public class Block {
 
     // ===== fields =====
 
+    /**
+     * This is used to identify this {@code Block} from others.
+     */
     protected String identifier;
+    /**
+     * the start time for this {@code Block}
+     */
+    protected long startTime;
+    /**
+     * the end time for this {@code Block}
+     */
+    protected long endTime;
+
     private int blockId;
 
     // ===== static fields =====
@@ -15,6 +27,26 @@ public class Block {
 
     public Block(String id) {
 	identifier = id;
+
+	long time = System.currentTimeMillis();
+	time = time - time % (1000 * 60); // round down to the nearest minute
+
+	startTime = time;
+	endTime = time + 1000 * 60 * 60;
+
+	blockId = nextBlockId++;
+    }
+
+    public Block(String id, long start) {
+	this(id, start, start + 3600000); // add an hour to the end
+    }
+
+    public Block(String id, long start, long end) {
+	identifier = id;
+
+	startTime = start - start % (1000 * 60);
+	endTime = end - end % (1000 * 60);
+
 	blockId = nextBlockId++;
     }
 
@@ -24,7 +56,25 @@ public class Block {
 	return identifier;
     }
 
-    // ===== overrides=====
+    public long getStartTime() {
+	return startTime;
+    }
+
+    public long getEndTime() {
+	return endTime;
+    }
+
+    // ===== modifiers =====
+
+    public void setStartTime(long newTime) {
+	startTime = newTime;
+    }
+
+    public void setEndTime(long newTime) {
+	endTime = newTime;
+    }
+
+    // ===== overrides =====
 
     @Override
     public int hashCode() {
@@ -63,8 +113,11 @@ public class Block {
 
     @Override
     public String toString() {
-	return "Block [blockId=" + blockId + ", "
-		+ (identifier != null ? "identifier=" + identifier : "") + "]";
+	String ret = (identifier != null ? identifier : "");
+	if (identifier != null && startTime != 0) {
+	    ret += ": " + startTime + (endTime != 0 ? " - " + endTime : "");
+	}
+	return ret;
     }
 
 }
